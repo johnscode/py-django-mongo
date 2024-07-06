4b35c19324ca62a79d015936450b7c23b46cf87e
John F <john@johnscode.com>
2024-07-06 18:49:04 -0500

    add mongo read, user count to home

--- root/views.py
+++ root/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from mongo_utils import get_collection_handle
 
+
 def home(request):
     collection = get_collection_handle('users')
-    count = collection.count_documents({})
-    return render(request, 'home.html')
+    context = {
+        'user_count' : collection.count_documents({}),
+    }
+    return render(request, 'home.html', context)
 
